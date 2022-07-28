159ad5b2944b68794a650858c3de93fc871693ea
MarkViews <46404728+markviews@users.noreply.github.com>
2022-07-27 22:37:19 -0700

    formating
    bluescreem's code confuses me..

--- FriendNotesCVR/FriendNotes.cs
+++ FriendNotesCVR/FriendNotes.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 using FriendNotesCVR;
 using cohtml;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System.Globalization;
 
 [assembly: MelonInfo(typeof(FriendNotes), "FriendNotes", "1.0.0", "MarkViews")]
 [assembly: MelonGame("Alpha Blend Interactive", "ChilloutVR")]
@@ -12,49 +14,48 @@ namespace FriendNotesCVR {
 
         /* TODO
          * 
-         * Can publish mod after these 2 work, the more features the better tho:
          * Add 'Edit Note' button to social page
          * Add note to social page
-         * 
          * Add note to nameplate
          * Log add Friend event
          * Log friend display names
-         * 
-         * Review code.. I copy pasted from my old mod. ofc I will credit Bluscream in the new mod
-         * (old FriendNotes had 'Date' and 'DateFirstSeen' for each logged DisplayName which was kinda redundent, lets get rid of one of them. This was probably a bug)
          * 
          * possibly add support for coloring notes / date added friend on nameplate.. low priority
          */
 
         public static FileInfo notesFile = new FileInfo("UserData/FriendNotes.json");
-        public static MelonPreferences_Category cat;
         public static Dictionary<string, UserNote> notes;
-
         public static bool showNotesOnNameplates;
         public static bool logDate;
         public static bool showDateOnNameplates;
         public static bool logName;
         public static string dateFormat;
-        public static bool notesAtBioTopNotBottom;
+
+        public static JsonSerializerSettings JsonSettings = new JsonSerializerSettings
+        {
+            MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
+            DateParseHandling = DateParseHandling.None,
+            Converters = { new IsoDateTimeConverter { DateTimeStyles = DateTimeStyles.AssumeUniversal } },
+            Formatting = Formatting.Indented
+        };
 
         public override void OnApplicationStart() {
-            cat = MelonPreferences.CreateCategory("FriendNotes", "Friend Notes");
+            MelonPreferences_Category cat = MelonPreferences.CreateCategory("FriendNotes", "Friend Notes");
             cat.CreateEntry("showNotesOnNameplates", true, "Show notes on nameplates");
             cat.CreateEntry("showDateOnNameplates", true, "Show date on nameplates");
             cat.CreateEntry("logDate", true, "Log date you add friends");
             cat.CreateEntry("logName", true, "Log friend display names");
             cat.CreateEntry("dateFormat", "M/d/yy - hh:mm tt");
 
-
-
+            notes = loadNotes();
         }
 
         public override void OnPreferencesSaved() {
-            showNotesOnNameplates = MelonPreferences.GetEntryValue<bool>(cat.Identifier, "showNotesOnNameplates");
-            logDate = MelonPreferences.GetEntryValue<bool>(cat.Identifier, "logDate");
-            logName = MelonPreferences.GetEntryValue<bool>(cat.Identifier, "logName");
-            showDateOnNameplates = MelonPreferences.GetEntryValue<bool>(cat.Identifier, "showDateOnNameplates");
-            dateFormat = MelonPreferences.GetEntryValue<string>(cat.Identifier, "dateFormat");
+            showNotesOnNameplates = MelonPreferences.GetEntryValue<bool>("FriendNotes", "showNotesOnNameplates");
+            logDate = MelonPreferences.GetEntryValue<bool>("FriendNotes", "logDate");
+            logName = MelonPreferences.GetEntryValue<bool>("FriendNotes", "logName");
+            showDateOnNameplates = MelonPreferences.GetEntryValue<bool>("FriendNotes", "showDateOnNameplates");
+            dateFormat = MelonPreferences.GetEntryValue<string>("FriendNotes", "dateFormat");
         }
 
 
@@ -100,30 +101,21 @@ namespace FriendNotesCVR {
         }
 
         public static void saveNotes() {
-            notesFile.Directory.Create();
             if (!notesFile.Exists) notesFile.Create().Close();
-            File.WriteAllText(notesFile.FullName, JsonConvert.SerializeObject(notes, FriendNotesCVR.Converter.Settings));
+            File.WriteAllText(notesFile.FullName, JsonConvert.SerializeObject(notes, JsonSettings));
         }
 
-        public static Dictionary<string, UserNote> loadNotes()
-        {
-            if (notesFile.Exists)
-            {
-                try
-                {
-                    notes = UserNotes.FromFile(notesFile);
-                    return notes;
-                }
-                catch (Exception ex)
-                {
+        public static Dictionary<string, UserNote> loadNotes() {
+            if (notesFile.Exists) {
+                try {
+                    return JsonConvert.DeserializeObject<Dictionary<string, UserNote>>(File.ReadAllText(notesFile.FullName), JsonSettings);
+                } catch (Exception ex) {
                     MelonLogger.Error($"Failed to load notes from {notesFile.FullName}:\n\t{ex.Message}");
                     //notesFile.Backup(true, ".corrupt");
                 }
             }
-            notes = new Dictionary<string, UserNote>();
-            return notes;
+            return new Dictionary<string, UserNote>();
         }
-
 
     }
 }
--- FriendNotesCVR/UserNote.cs
+++ FriendNotesCVR/UserNote.cs
@@ -1,117 +1,29 @@
-﻿using System.Globalization;
-using System.Text;
-using ABI_RC.Core.Player;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Converters;
+﻿using Newtonsoft.Json;
 
 namespace FriendNotesCVR {
-    
-    public static class UserNotes
-    {
-        public static Dictionary<string, UserNote> FromFile(FileInfo file) => FromJson(File.ReadAllText(file.FullName));
-        public static Dictionary<string, UserNote> FromJson(string json) => JsonConvert.DeserializeObject<Dictionary<string, UserNote>>(json, Converter.Settings);
-    }
 
-    public partial class UserNote
-    {
+    public partial class UserNote {
 
-        [JsonIgnore]
-        public string DisplayName { get { return DisplayNames.Last()?.Name; } }
+        public string Note;
+        public List<DisplayName> DisplayNames;
 
-        [JsonIgnore]
-        public string FullText { get {
-                StringBuilder sb = new StringBuilder();
-                if (HasNote) sb.AppendLine(Note);
-                if (HasDate) sb.AppendLine(DateAddedText);
-                return sb.ToString();
-            } }
-
-        [JsonIgnore]
-        public bool HasNote { get { return !string.IsNullOrWhiteSpace(Note); } }
-        [JsonProperty("Note", NullValueHandling = NullValueHandling.Ignore)]
-        public string Note { get; set; }
-
-        [JsonIgnore]
-        public bool HasDate { get { return DateAdded != null; } }
-        [JsonIgnore]
-        public string DateAddedText { get { return HasDate ? "Added: " + DateAdded?.ToString(FriendNotes.dateFormat) : string.Empty; } }
-        [JsonProperty("DateAdded", NullValueHandling = NullValueHandling.Ignore)]
-        public DateTime? DateAdded { get; set; }
-
-        [JsonProperty("DateRequested", NullValueHandling = NullValueHandling.Ignore)]
-        public DateTime? DateRequested { get; set; }
-
-        [JsonProperty("DisplayNames", NullValueHandling = NullValueHandling.Ignore)]
-        public List<DisplayName> DisplayNames { get; set; }
-
-        public UserNote Update(string displayname = null)
-        {
-            if (displayname != null && FriendNotes.logName)
-            {
+        public void LogDisplayName(string displayname) {
+            if (displayname != null && FriendNotes.logName) {
                 if (DisplayNames is null) DisplayNames = new List<DisplayName>();
                 var names = DisplayNames.Where(f => f.Name == displayname);
-                if (names.Count() < 1) DisplayNames.Add(new DisplayName(displayname, DateTime.Now));
-                else names.Last().DateFirstSeen = DateTime.Now;
+                if (names.Count() < 1) DisplayNames.Add(new DisplayName(displayname));
             }
-            return this;
         }
     }
 
-    public partial class DisplayName
-    {
-        [JsonProperty("Name", NullValueHandling = NullValueHandling.Ignore)]
-        public string Name { get; set; }
+    public partial class DisplayName {
+        public string Name;
+        public DateTime DateFirstSeen;
 
-        [JsonProperty("Date", NullValueHandling = NullValueHandling.Ignore)]
-        public DateTime? Date { get; set; }
-
-        [JsonProperty("DateFirstSeen", NullValueHandling = NullValueHandling.Ignore)]
-        public DateTime? DateFirstSeen { get; set; }
-
-        public DisplayName(string name, DateTime dateFirstSeen)
-        {
+        public DisplayName(string name) {
             Name = name;
-            DateFirstSeen = dateFirstSeen;
-            
-        }
-
-        public override string ToString()
-        {
-            return $"\"{Name}\" ({DateFirstSeen})";
+            DateFirstSeen = DateTime.Now;
         }
     }
 
-    public static partial class Extensions
-    {
-
-        public static UserNote AddPlayer(this Dictionary<string, UserNote> list, CVRPlayerEntity player) => list.AddPlayer(player);
-        public static UserNote AddPlayer(this Dictionary<string, UserNote> list, string userid, string displayName = null)
-        {
-            var player = new UserNote();
-            if (displayName != null) player.DisplayNames = new List<DisplayName>() { new DisplayName(displayName, DateTime.Now) };
-            list[userid] = player;
-            return player;
-        }
-        public static UserNote AddOrUpdate(this Dictionary<string, UserNote> list, CVRPlayerEntity player) => list.AddOrUpdate(player);
-        public static UserNote AddOrUpdate(this Dictionary<string, UserNote> list, string userid, string displayname = null)
-        {
-            if (list.ContainsKey(userid)) return list[userid].Update(displayname);
-            return list.AddPlayer(userid, displayname);
-        }
-
-    }
-
-    internal static class Converter
-    {
-        public static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
-        {
-            MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
-            DateParseHandling = DateParseHandling.None,
-            Converters =
-            {
-                new IsoDateTimeConverter { DateTimeStyles = DateTimeStyles.AssumeUniversal }
-            },
-            Formatting = Formatting.Indented
-        };
-    }
 }
